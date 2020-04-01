877a12819f422075b983d35bc826947c496037d9
Ian Dennis Miller <ian@iandennismiller.com>
2020-04-01 11:17:40 -0400

    automatic publish

--- docs/js/search.js
+++ docs/js/search.js
@@ -1,26 +1,40 @@
 var searchIndex;
 var results_node = document.getElementById("results");
+var citations;
 
 var indexLoaded = function (e) {
     var resp = pako.inflate(e.target.response, { to: 'string' });
     var data = JSON.parse(resp);
     searchIndex = lunr.Index.load(data);
+    document.getElementById("wait").style.display = "none";
     document.getElementById("search").placeholder = "ready to search";
     document.getElementById("search").disabled = false;
     document.getElementById("search").focus();
-    document.getElementById("wait").remove();
+}
+
+var citationsLoaded = function (e) {
+    var resp = pako.inflate(e.target.response, { to: 'string' });
+    citations = JSON.parse(resp);
 }
 
 var search = function(query) {
     results = searchIndex.search(query);
+    results_node.innerHTML = "";
+
     for (var idx in results) {
+        var cite_data = citations[results[idx].ref];
+
         var link_node = document.createElement("a");
-        var link_text = document.createTextNode(results[idx].ref);
-        link_node.appendChild(link_text)
-        link_node.href = "/articles/" + results[idx].ref;
+        link_node.appendChild(document.createTextNode(cite_data['t']));
+        link_node.href = "/articles/" + results[idx].ref.toLowerCase() + ".html";
+        link_node.className = "search-result";
+
+        var meta_node = document.createElement("p");
+        meta_node.appendChild(document.createTextNode(cite_data['a'] + ". (" + cite_data['y'] + "). " + cite_data['j'] + "."));
 
         var list_node = document.createElement("LI");
         list_node.appendChild(link_node);
+        list_node.appendChild(meta_node);
 
         results_node.appendChild(list_node);
         // console.log(results[idx]);
@@ -30,6 +44,13 @@ var search = function(query) {
             break;
         }
     }
+
+    if (results.length == 0) {
+        var list_node = document.createElement("LI");
+        list_node.appendChild(document.createTextNode("No results"));
+        results_node.appendChild(list_node);
+    }
+
 }
 
 var search_from_field = function() {
@@ -42,6 +63,12 @@ xhr.responseType = "arraybuffer";
 xhr.open('get', '/data/index.json.gz');
 xhr.addEventListener('load', indexLoaded);
 xhr.send();
+
+var xhr_citations = new XMLHttpRequest;
+xhr_citations.responseType = "arraybuffer";
+xhr_citations.open('get', '/data/citations.json.gz');
+xhr_citations.addEventListener('load', citationsLoaded);
+xhr_citations.send();
 
 var button_node = document.getElementById("search_button");
 button_node.addEventListener("click", search_from_field); 
